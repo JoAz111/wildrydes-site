f80521e2376933b492871bc1b7ec9729364f0941
Joey A <joeyazizoff@gmail.com>
2021-11-12 17:32:38 -0800

    commit

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_9uhcrMTPY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7q6qbl4kk6tmeaej05aqmk6e0e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
